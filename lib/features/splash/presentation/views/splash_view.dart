bdb0d8f7a8903e6f94b7912f90c5a83242554f7c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-12-22 13:59:14 +0200

    Finish Splash feature's UI

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:social_app/config/router/routes.dart';
+import 'package:social_app/core/helpers/cache_helper.dart';
+import 'package:social_app/core/helpers/helper.dart';
+import 'package:social_app/core/utils/app_strings.dart';
+import 'package:social_app/features/splash/presentation/widgets/splash_view_body.dart';
+import 'package:social_app/service_locator.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  late Timer _timer;
+
+  @override
+  void initState() {
+    // _startDelay();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startDelay() {
+    _timer = Timer(const Duration(milliseconds: 2500), () => _goToNext());
+  }
+
+  void _goToNext() {
+    // Helper.uId = getIt.get<CacheHelper>().getStringData(key: AppStrings.uId);
+
+    // if (Helper.uId != null) {
+    //   context.navigateAndReplacement(newRoute: Routes.layoutRoute);
+    // } else {
+    //   context.navigateAndReplacement(newRoute: Routes.loginRoute);
+    // }
+  }
+
+  void _setSystemUIOverlayStyle() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    final double bottomPadding = View.of(context).viewPadding.bottom;
+
+    // Set the color based on the presence of the system navigation bar
+    final Color? systemNavigationBarColor =
+        bottomPadding > 0 ? null : Colors.transparent;
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: systemNavigationBarColor,
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    _setSystemUIOverlayStyle();
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: SplashViewBody(),
+    );
+  }
+}
--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,13 +1,59 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:social_app/core/utils/app_assets.dart';
+import 'package:social_app/features/splash/presentation/widgets/faded_widget.dart';
+import 'package:video_player/video_player.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(AppAssets.appIconVideo)
+      ..initialize().then((_) {
+        setState(() {
+          // Ensure the first frame is shown after the video is initialized
+        });
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(AppAssets.appIcon),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : const CircularProgressIndicator(),
+        const FadedWidget(
+          child: Text(
+            'Linkup',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
