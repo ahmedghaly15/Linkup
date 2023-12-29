beb0aa232a9736b2d40dc199d952bdad2392cfbb
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-12-29 09:23:00 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ import 'package:social_app/service_locator.dart';
 
 import 'package:social_app/linkup_app.dart';
 
-// TODO: use create post method when you upload the profile image or cover image to share a new post like in facebook
-// TODO: Add functionality to tags TextButton in NewPostView
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
