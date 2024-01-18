3ac3cc77e09f43467bbe87f61ea61da0c5a22b83
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-18 13:15:36 +0200

    Refactor ForgotPassword feature's UI

--- lib/features/auth/presentation/views/forgot_password_view.dart
+++ lib/features/auth/presentation/views/forgot_password_view.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:social_app/config/router/routes.dart';
 import 'package:social_app/config/themes/app_colors.dart';
-import 'package:social_app/core/utils/app_constants.dart';
 import 'package:social_app/core/utils/app_navigator.dart';
 import 'package:social_app/config/themes/app_text_styles.dart';
-import 'package:social_app/core/widgets/get_back_arrow.dart';
+import 'package:social_app/core/widgets/custom_content_container.dart';
+import 'package:social_app/core/widgets/custom_filling_container.dart';
+import 'package:social_app/core/widgets/custom_get_back_button.dart';
 import 'package:social_app/features/auth/presentation/widgets/auth_view_sub_title.dart';
 import 'package:social_app/features/auth/presentation/widgets/forgot_password/forgot_password_form.dart';
 import 'package:social_app/features/auth/presentation/widgets/have_account_or_not_text.dart';
@@ -16,33 +17,42 @@ class ForgotPasswordView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const GetBackArrow(),
-      ),
-      body: Padding(
-        padding: AppConstants.horizontalPadding,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'FORGOT PASSWORD',
-              style: AppTextStyles.textStyle28Bold.copyWith(
-                color: AppColors.primaryColor,
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: CustomFillingContainer(
+              child: CustomContentContainer(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const CustomGetBackButton(hasShadow: true),
+                    const Spacer(),
+                    Text(
+                      'FORGOT PASSWORD',
+                      style: AppTextStyles.textStyle28Bold.copyWith(
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                    const AuthViewSubTitle(
+                      subTitle:
+                          'Enter your account to send you a reset password mail',
+                    ),
+                    SizedBox(height: 30.h),
+                    const ForgotPasswordForm(),
+                    HaveAccountOrNotText(
+                      question: "Don't have an account?",
+                      buttonText: 'Sign up',
+                      onTap: () =>
+                          context.navigateTo(routeName: Routes.signUpRoute),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
               ),
             ),
-            const AuthViewSubTitle(
-              subTitle: 'Enter your account to send you a reset password mail',
-            ),
-            SizedBox(height: 30.h),
-            const ForgotPasswordForm(),
-            HaveAccountOrNotText(
-              question: "Don't have an account?",
-              buttonText: 'Sign up',
-              onTap: () => context.navigateTo(routeName: Routes.signUpRoute),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
