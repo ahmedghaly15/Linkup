1d0741fba659c0700f479ceea7d942b6d6fb59e8
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-26 00:03:44 +0200

    Update app logo for android & IOS

---
+++ lib/features/users/domain/usecases/get_user_posts.dart
@@ -0,0 +1,16 @@
+// import 'package:dartz/dartz.dart';
+// import 'package:social_app/core/errors/failure.dart';
+// import 'package:social_app/core/usecases/base_usecase.dart';
+// import 'package:social_app/features/posts/data/models/post_model.dart';
+// import 'package:social_app/features/users/domain/repositories/user_repo.dart';
+
+// class GetUserPostsUseCase implements BaseUseCases<List<PostModel>, String> {
+//   final UserRepo userRepo;
+
+//   const GetUserPostsUseCase({required this.userRepo});
+
+//   @override
+//   Future<Either<Failure, List<PostModel>>> call(String params) async {
+//     return await userRepo.getUserPosts(uId: params);
+//   }
+// }
