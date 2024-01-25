270db2cb7ee3b228717f9a942b5357fa64c0ffcb
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-25 22:35:04 +0200

    Use getUserPosts() when profile icon is tapped

--- lib/features/linkup/presentation/cubits/linkup_cubit.dart
+++ lib/features/linkup/presentation/cubits/linkup_cubit.dart
@@ -2,11 +2,13 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/core/entities/no_params.dart';
+import 'package:social_app/core/helpers/helper.dart';
 import 'package:social_app/features/linkup/domain/entities/change_index_params.dart';
 import 'package:social_app/features/linkup/domain/usecases/change_bottom_nav_index.dart';
 import 'package:social_app/features/linkup/domain/usecases/change_nav_to_home.dart';
 import 'package:social_app/features/linkup/domain/usecases/get_body.dart';
 import 'package:social_app/features/linkup/domain/usecases/get_bottom_nav_items.dart';
+import 'package:social_app/features/users/presentation/cubits/user_profile/user_profile_cubit.dart';
 
 part 'linkup_state.dart';
 
@@ -40,9 +42,9 @@ class LinkupCubit extends Cubit<LinkupState> {
 
     // if (currentIndex == 0) BlocProvider.of<UserCubit>(context).getPosts();
 
-    // if (currentIndex == 3) {
-    //   BlocProvider.of<UserProfileCubit>(context).getUserPosts(uId: Helper.uId!);
-    // }
+    if (currentIndex == 3) {
+      BlocProvider.of<UserProfileCubit>(context).getUserPosts(uId: Helper.uId!);
+    }
 
     emit(ChangeBottomNavIndex(index: currentIndex));
   }
